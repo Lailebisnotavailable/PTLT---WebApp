033a1d6a88d9528f66ee43a649cbd5dd79cced2b
Batshelly <sjpotpot13@gmail.com>
2025-06-26 17:01:37 +0800

    reroute ng urls.py

--- PTLT/urls.py
+++ PTLT/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('PTLT_App/', include('PTLT_App.urls')),
+    path('', include('PTLT_App.urls')),
     path('admin/', admin.site.urls),
 ]
