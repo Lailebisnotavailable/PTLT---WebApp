6ab89e5bacafff05167f016612df73293bceb4d6
Batshelly <sjpotpot13@gmail.com>
2025-06-26 17:46:19 +0800

    added 3 htmls

--- PTLT_App/urls.py
+++ PTLT_App/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    
+    path('manage_user', views.manage_user, name='manage_user'),
+    path('attendance_logs', views.attendance_logs, name='attendance_logs'),
+    path('instructor_schedule', views.instructor_schedule, name='instructor_schedule'),
 ]
--- PTLT_App/views.py
+++ PTLT_App/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def login(request):
     return render(request, 'login.html')
 
+def manage_user(request):
+    return render(request, 'manage_user.html')
 
+def attendance_logs(request):
+    return render(request, 'attendance_logs.html')
 
+def instructor_schedule(request):
+    return render(request, 'instructor_schedule.html')
